f9b1c17ca65a00aff94a3ee72e96adb9b33e0430
sid <siddhanth@wankhade.com>
2024-01-19 21:50:25 +0530

    make add to cart interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -49,7 +49,8 @@ products.forEach((product)=>{
             Added
         </div>
 
-        <button class="add-to-cart-button button-primary">
+        <button class="add-to-cart-button button-primary js-add-to-cart"
+           data-product-id="${product.id}">
             Add to Cart
         </button>
         </div>`;
@@ -61,3 +62,28 @@ console.log(productsHtml);
 
 
 document.querySelector('.jsproducts-grid').innerHTML = productsHtml;
+
+
+document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
+    button.addEventListener('click',()=>{
+       const productId= button.dataset.productId;//product-name converts to productName(from kebab to camel) 
+       let matchingItem;
+       cart.forEach((item)=>{
+         if(productId === item.productId){
+           matchingItem=item;
+         }
+       });
+       if(matchingItem){
+        matchingItem.quantity+=1;
+       } else{
+        cart.push({
+            productId: productId,
+            quantity:1
+           });
+       }
+
+       
+       console.log(cart);   
+    });
+
+});
