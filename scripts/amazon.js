7693e4f2cf240d60f3960b90aa68bc410dc9eac9
sid <siddhanth@wankhade.com>
2024-01-19 21:59:58 +0530

    make the cart quantity interactive

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -81,9 +81,14 @@ document.querySelectorAll('.js-add-to-cart').forEach((button)=>{
             quantity:1
            });
        }
-
        
-       console.log(cart);   
+       let cartQuantity=0;
+       cart.forEach((item)=>{
+        cartQuantity+=item.quantity;
+       });
+       console.log(cartQuantity);   
+       document.querySelector('.js-cart-quantity').innerHTML=cartQuantity;
     });
+
 
 });
