4a4eb175bcbf57624530d012ab80158d3c8ce4a1
sid <siddhanth@wankhade.com>
2024-01-20 19:12:56 +0530

    save cart in localstorage

--- data/cart.js
+++ data/cart.js
@@ -1,12 +1,16 @@
-export let cart =[{
-    productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
-    quantity: 1
-},{
-    productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
-    quantity: 1
+export let cart = JSON.parse(localStorage.getItem('cart'));
+  if(!cart){
+    cart = [{
+        productId:'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+        quantity: 1
+    },{
+        productId:'15b6fc6f-327a-4ec4-896f-486349e85a3d',
+        quantity: 1
+    
+    }
+    ];
+  }
 
-}
-];
 
 
 
@@ -25,8 +29,13 @@ export function addToCart(productId){
          quantity:1
         });
     }
+    saveToStorage();
 }
 
+
+function saveToStorage(){
+    localStorage.setItem('cart',JSON.stringify(cart))
+}
 
 export function removeFromCart(productId){
     const newCart=[];
@@ -36,4 +45,5 @@ export function removeFromCart(productId){
       }
     });
    cart= newCart;
+   saveToStorage();
 }
